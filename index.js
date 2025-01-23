3efe717bacecf42596653b610b90b6e1f6077d1e
Irungusam <irungusam8@gmail.com>
2025-01-23 12:37:24 +0300

    add my first server to github

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+const port = 3000;
+app.use(express.json());
+
+let itemData = [];
+let nextId = 1;
+
+app.post("/", (req, res) => {
+  const { name, price } = req.body;
+  const newItem = { id: nextId++, name, price };
+  itemData.push(newItem);
+  res.status(201).send(newItem);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}...`);
+});
